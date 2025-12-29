6b6b7acef88bb3692a97ee92108d5d8840bf0c78
138-jim <71417497+138-jim@users.noreply.github.com>
2025-12-29 14:54:18 +0930

    Update test_patterns.py

--- rpi_driver/test_patterns.py
+++ rpi_driver/test_patterns.py
@@ -545,8 +545,8 @@ def snow(width: int, height: int, offset: float = 0) -> np.ndarray:
     for y in range(height):
         for x in range(width):
             # Slightly lighter than rain for snowy day
-            brightness = int(3 + (y / height) * 2)
-            frame[y, x] = [brightness // 2, brightness // 2, brightness // 2]
+            brightness = int(3 + (y / height) * 3)
+            frame[y, x] = [brightness // 2, brightness // 2, (brightness + 10) // 2]
 
     # Number of snowflakes
     num_flakes = 40
